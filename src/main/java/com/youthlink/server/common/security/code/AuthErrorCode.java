979d9979ce9f0eca9d011059c4e8e2095069367c
twodo0 <dopal0426@gmail.com>
2026-03-13 00:22:29 +0900

    [FEAT] 보안 예외 처리 및 에러  코드 추가

---
+++ src/main/java/com/youthlink/server/common/security/code/AuthErrorCode.java
@@ -0,0 +1,16 @@
+package com.youthlink.server.common.security.code;
+
+import com.youthlink.server.common.apipayload.code.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum AuthErrorCode implements BaseErrorCode {
+    UNSUPPORTED_SOCIAL_PROVIDER(HttpStatus.BAD_REQUEST, "AUTH400", "지원하지 않는 소셜 로그인 서비스입니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
