69d8580815ac907dd6a24b69a9dbacbc16136a95
twodo0 <dopal0426@gmail.com>
2026-03-12 23:46:55 +0900

    [FEAT] 소셜 로그인 연동 위한 회원 가입 및 조회 로직 추가

--- src/main/java/com/youthlink/server/domain/member/service/MemberCommandService.java
+++ src/main/java/com/youthlink/server/domain/member/service/MemberCommandService.java
@@ -2,7 +2,10 @@ package com.youthlink.server.domain.member.service;
 
 import com.youthlink.server.domain.member.dto.MemberReqDto;
 import com.youthlink.server.domain.member.dto.MemberResDto;
+import com.youthlink.server.domain.member.entity.Member;
 
 public interface MemberCommandService {
     MemberResDto.MemberDetailDto updateProfile(Long memberId, MemberReqDto.ProfileUpdateDto request);
+
+    Member getOrCreateMember(String email, String name);
 }
