0011a1a7faf65b513f2eae4adb93391fe8e71dbb
twodo0 <dopal0426@gmail.com>
2026-03-13 15:27:31 +0900

    [CHORE]: 전역 응답 코드 표준화

--- src/main/java/com/youthlink/server/domain/member/code/MemberErrorCode.java
+++ src/main/java/com/youthlink/server/domain/member/code/MemberErrorCode.java
@@ -8,8 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum MemberErrorCode implements BaseErrorCode {
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER4001", "사용자를 찾을 수 없습니다."),
-    MEMBER_ALREADY_EXISTS(HttpStatus.CONFLICT, "MEMBER4002", "이미 존재하는 사용자입니다.");
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER404", "사용자를 찾을 수 없습니다."),
+    MEMBER_ALREADY_EXISTS(HttpStatus.CONFLICT, "MEMBER409", "이미 존재하는 사용자입니다.");
 
     private final HttpStatus status;
     private final String code;
